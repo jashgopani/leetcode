4602d73034bee35bb3127029d824cd43d707e432
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-03-02 17:28:33 -0500

    Time: 1 ms (84.68%) | Memory: 41.2 MB (98.27%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -5,12 +5,13 @@ class Solution {
         int m = word2.length();
         String longer = n>m?word1:word2;
         int limit = Math.min(word1.length(),word2.length());
-        for(int i=0;i<limit;i++){
-          sb.append(word1.charAt(i));
-          sb.append(word2.charAt(i));
-        }
-        for(int i=limit;i<longer.length();i++){
-          sb.append(longer.charAt(i));
+        for(int i=0;i<longer.length();i++){
+          if(i<limit){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+          }else{
+            sb.append(longer.charAt(i));
+          }
         }
         return sb.toString();
     }
