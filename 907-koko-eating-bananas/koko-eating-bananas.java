2bbc7d9c144f5c1c9eb9e8da2a4ea44a31c5809e
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-05-24 03:21:16 -0400

    Time: 270 ms (5.19%) | Memory: 46.9 MB (5.91%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+      Arrays.sort(piles);
+      int n = piles.length;
+      int left = 1, right = piles[n-1];
+      int k = left;
+      int speed = piles[n-1];
+
+      while(left<=right && k>0){
+        k = ((left+right)/2);
+        long time = getEatingTime(k,piles);
+        if(time <= h){
+          speed = Math.min(speed,k);
+          right = k-1;
+        }else{
+          left = k+1;
+        }
+        System.out.println("New> left: "+left+" right: "+right+" Speed: "+speed+" k: "+k);
+      }
+
+      return speed;
+    }
+
+    public boolean inRange(int x, int l, int r){
+      return x>=l && x<=r;
+    }
+
+    public long getEatingTime(double speed,int[] piles){
+      long time = 0;
+      List<Integer> times = new ArrayList<>();
+      for(int p:piles){
+        int t = (int)Math.max(1,Math.ceil(p/speed)); 
+        time+=t;
+        times.add(t);
+      }
+
+      System.out.println("Current> Speed: "+speed+" Times: "+times+" Sum: "+time);
+      return time;
+    }
+}
