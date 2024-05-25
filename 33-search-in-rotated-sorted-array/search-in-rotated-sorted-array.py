a5ad1f10965c331b8e39ebe0a32df20ea673b9c5
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-05-25 19:52:27 -0400

    Time: 39 ms (80.87%) | Memory: 16.9 MB (53.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -9,20 +9,15 @@ class Solution:
           if l==r and mid!=target:
             break
 
-          print(f"checking: ({m} in [{l}, {r}]) -> [{nums[l]},{nums[r]}], mid: {mid}")
           if mid==target:
             return m
           
           if nums[l] < nums[r]:
-            print('sorted case, doing normal bs')
             if target > mid:
-              print('sorted case, going to right half next')
               l = m+1
             else:
-              print('sorted case, going to left half next')
               r = m-1
           elif nums[l] <= mid:
-            print('rotated case: mid in left subarray')
             if target < mid:
               if target >= nums[l]:
                 r = m-1
@@ -31,7 +26,6 @@ class Solution:
             else:
               l = m+1
           else:
-            print('rotated case: mid in right subarray')
             if target < mid:
               r = m-1
             elif target > nums[r]:
