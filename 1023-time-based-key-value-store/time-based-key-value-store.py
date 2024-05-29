7faf9fdfffd350ca0c05fe675781d3118c0930f5
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-05-29 00:16:20 -0400

    Time: 633 ms (12.15%) | Memory: 73.5 MB (83.22%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,40 +1,32 @@
-import heapq
-from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
       self.map = {}       
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-      # print(f"\nset: {key},{timestamp} -> {value}")
       if key not in self.map:
         self.map[key] = [(timestamp,value)]
-        heapq.heapify(self.map[key])
       else:
-        heapq.heappush(self.map[key],(timestamp,value))
-      # print(f"after set: {self.map}\n")
+        self.map[key].append((timestamp,value))
 
     def get(self, key: str, timestamp: int) -> str:
       res = ""
       if key in self.map and len(self.map[key])>0:
         records = self.map[key]
-        # print(f"\nget({key},{timestamp}) -> {records}")
         l, r = 0, len(records)-1
         while l <= r:
           m = (l+r)//2
-          # print("l,r,m,record",l,r,m, records[m])
           ts, value = records[m][0], records[m][1]
+          
           if ts <= timestamp:
             res = value
 
           if l==r:
-            # print(f"l==r @ {records[m]}")
             break
           
           if ts==timestamp:
             return value
           elif timestamp > ts:
-            res = value
             l = m+1
           else:
             r = m-1
