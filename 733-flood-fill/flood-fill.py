914151a3088407dc87458f1662917d3a994f1d0f
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-08 15:02:40 -0400

    Time: 72 ms (21.95%) | Memory: 16.7 MB (69.88%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,5 +1,8 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        return self.dfs(image,sr,sc,color) if image[sr][sc] != color else image
+    
+    def bfs(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         target = image[sr][sc]
         if target==color: return image
         queue = [(sr,sc)]
@@ -15,6 +18,50 @@ class Solution:
                 queue.append((i+1,j)) #bottom
             if j-1 >=0 and image[i][j-1]==target:
                 queue.append((i,j-1)) #left
+
+        return image
+    
+    def dfs(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        target = image[sr][sc]
+        if target==color: return image
+        stack = [(sr,sc)]
+        m = len(image)
+        n = len(image[0])
+        image[sr][sc]=color
+
+        while stack:
+            i,j = stack.pop()    
+            # top
+            for r in range(i-1,-1,-1):
+                if image[r][j] == target:
+                    image[r][j] = color
+                    stack.append((r,j))
+                else:
+                    break
+            
+            # right
+            for c in range(j+1, n):
+                if image[i][c] == target:
+                    image[i][c] = color
+                    stack.append((i,c))
+                else:
+                    break
+
+            # bottom
+            for r in range(i+1,m):
+                if image[r][j] == target:
+                    image[r][j] = color
+                    stack.append((r,j))
+                else:
+                    break
+            
+            # right
+            for c in range(j-1, -1,-1):
+                if image[i][c] == target:
+                    image[i][c] = color
+                    stack.append((i,c))
+                else:
+                    break
 
         return image
         
