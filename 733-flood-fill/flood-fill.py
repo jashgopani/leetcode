e49820ee76676f9739ef193016dfc6267603d7c8
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-08 13:06:53 -0400

    Time: 83 ms (5.77%) | Memory: 16.6 MB (93.52%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        target = image[sr][sc]
+        if target==color: return image
+        queue = [(sr,sc)]
+        while queue:
+            i, j = queue.pop(0)
+            image[i][j] = color
+            m = len(image)
+            n = len(image[0])
+            if i-1 >= 0 and image[i-1][j]==target:
+                queue.append((i-1,j)) #top
+            if j+1 < n and image[i][j+1]==target:
+                queue.append((i,j+1)) #right
+            if i+1 < m and image[i+1][j]==target:
+                queue.append((i+1,j)) #bottom
+            if j-1 >=0 and image[i][j-1]==target:
+                queue.append((i,j-1)) #left
+
+        return image
+        
