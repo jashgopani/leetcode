3ea66890c2b5b734afea81fcde7762c438a14b07
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-08 13:07:43 -0400

    Time: 78 ms (5.77%) | Memory: 16.5 MB (99.47%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -6,13 +6,12 @@ class Solution:
         while queue:
             i, j = queue.pop(0)
             image[i][j] = color
-            m = len(image)
-            n = len(image[0])
+
             if i-1 >= 0 and image[i-1][j]==target:
                 queue.append((i-1,j)) #top
-            if j+1 < n and image[i][j+1]==target:
+            if j+1 < len(image[0]) and image[i][j+1]==target:
                 queue.append((i,j+1)) #right
-            if i+1 < m and image[i+1][j]==target:
+            if i+1 < len(image) and image[i+1][j]==target:
                 queue.append((i+1,j)) #bottom
             if j-1 >=0 and image[i][j-1]==target:
                 queue.append((i,j-1)) #left
