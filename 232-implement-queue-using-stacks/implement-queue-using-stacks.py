520a41a6ecb4e355418bb265ce74f219166b6e15
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-08 19:48:34 -0400

    Time: 38 ms (32.41%) | Memory: 16.4 MB (92.06%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -7,41 +7,28 @@ class MyQueue:
         self.lastOpPush = True
 
     def push(self, x: int) -> None:
-        # print('push', x)
         if not self.lastOpPush:
             self.__swap__()
         self.queue[self.active].append(x)
-        self.printq()
         self.lastOpPush = True
 
     def pop(self) -> int:
-        # print('pop:')
         if self.lastOpPush:
             self.__swap__()
-        self.printq()
         self.lastOpPush = False
         return self.queue[self.active].pop()
 
     def peek(self) -> int:
-        # print('peek')
-        self.printq()
         top = 0 if self.lastOpPush else -1
         return self.queue[self.active][top] 
 
     def empty(self) -> bool:
-        # print('empty')
-        self.printq()
         return len(self.queue[self.active]) == 0
 
     def __swap__(self):
         self.active, self.inactive = self.inactive, self.active
         while len(self.queue[self.inactive]) > 0:
             self.queue[self.active].append(self.queue[self.inactive].pop())
-
-    def printq(self):
-        for q in range(len(self.queue)):
-            print("*" if q == self.active else " ",self.queue[q])
-        
         
         
 
