c22f31fe32f74ef9dc3df725ac375531ae6a78ad
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-10 18:23:16 -0400

    Time: 48 ms (83.77%) | Memory: 16.8 MB (18.85%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,15 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mDict = {}
-        for c in magazine:
-            mDict[c] = mDict.get(c,0)+1
-        
-        for c in ransomNote:
-            if c in mDict:
-                mDict[c] -= 1
-                if mDict[c]==0:
-                    del mDict[c]
-            else:
-                return False
-        
-        return True
+        str1 = Counter(ransomNote)
+        str2 = Counter(magazine)
+        if str1 & str2 == str1:
+            return True
+        return False
