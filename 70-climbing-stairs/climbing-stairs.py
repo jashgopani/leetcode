972f893b41ec90b538e3a6e6be03d8de51498e68
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-10 23:22:06 -0400

    Time: 38 ms (29.34%) | Memory: 16.5 MB (72.48%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [i if i<3 else -1 for i in range(0,n+1)]
-        return self.countWays(n,dp)
-    
-    def countWays(self, n, dp):
-        if n>2 and dp[n]==-1:
-            dp[n] = self.countWays(n-1,dp)+ self.countWays(n-2,dp)
-        
-        return dp[n]
+        first, second = 1, 1
+        for i in range(n-1):
+            temp = second
+            # first will become second for i+1
+            second = first
+            first = first + temp
+        return first
 
         
