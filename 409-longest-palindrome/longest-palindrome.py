f9a01e9922a4a0158db95f4611c4343a6b2e54f1
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-11 15:15:00 -0400

    Time: 33 ms (80.75%) | Memory: 16.6 MB (52.83%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,15 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        freq = Counter(s)
-        evenCount, oddCount = 0,0
-        for c,f in freq.items():
-            if f % 2 == 0:
-                evenCount += f
-            else:
-                evenCount += (f-1)
-                oddCount += 1
-
-        return evenCount + min(oddCount,1)
+        odd = 0
+        counter = {}
+        for c in s:
+            counter[c] = counter.get(c,0)+1
+            odd += (1 if counter[c]%2==1 else -1)
+        return len(s) - max(0, odd-1)
+            
         
                 
         
