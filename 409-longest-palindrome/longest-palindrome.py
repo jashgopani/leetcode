29290b6ea665ad45cc1977e11ebf23b205a21640
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-11 14:58:05 -0400

    Time: 41 ms (26.91%) | Memory: 16.5 MB (52.83%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -2,16 +2,13 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         freq = Counter(s)
         evenCount, oddCount = 0,0
-        # print(freq)
         for c,f in freq.items():
             if f % 2 == 0:
                 evenCount += f
-            elif f == 1:
-                oddCount += 1
             else:
                 evenCount += (f-1)
                 oddCount += 1
-        # print('even',evenCount,'odd',oddCount)
+
         res = evenCount
         res += (1 if oddCount >= 1 else 0)
         return res
