5546b27a74f2d99b4219b8f5e39fa726727ff276
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-18 23:23:16 -0400

    Time: 137 ms (5.21%) | Memory: 45.4 MB (5.62%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,10 +1,53 @@
-import java.math.BigInteger;
-
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger b1 = new BigInteger(a,2);
-        BigInteger b2 = new BigInteger(b,2);
-        BigInteger b3 = b1.add(b2);
-        return b3.toString(2);
+        int len = Math.max(a.length(),b.length())-1;
+        int ia = a.length()-1;
+        int ib = b.length()-1;
+        int s = 0, c=0;
+        StringBuilder sb = new StringBuilder(len+2);
+
+        while(len>=0){
+            int sum=c;
+            if(ia>=0){
+                sum+= (a.charAt(ia)- '0');
+                ia--;
+            }
+            if(ib>=0){
+                sum+= (b.charAt(ib) - '0');
+                ib--;
+            }
+            print("len:",len,"sum:",sum);
+            len--;
+            switch(sum){
+                case 1:
+                    sb.insert(0,1);
+                    c=0;
+                    break;
+                case 2:
+                    sb.insert(0,0);
+                    c=1;
+                    break;
+                case 3:
+                    sb.insert(0,1);
+                    c=1;
+                    break;
+                default://case 0
+                    c = 0;
+                    sb.insert(0,0);
+            }
+        }
+        if(c==1 || sb.length()==0)
+            sb.insert(0,c);
+        
+        return sb.toString();
+
     }
+
+    public static void print(Object... args){
+        for(Object o:args){
+            System.out.printf(o+" ");
+        }
+        System.out.println();
+    }
+
 }
