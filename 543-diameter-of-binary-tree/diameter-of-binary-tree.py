5da4c16085e68bc12ecd1625592b82a1c0c43b24
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-06-20 00:07:42 -0400

    Time: 53 ms (25.77%) | Memory: 19.6 MB (6.90%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -8,10 +8,10 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         return self.maxHeight(root)[2]
 
-    def maxHeight(self,root):
+    def maxHeight(self, root: Optional[TreeNode])->List[int]:
         if not root:
-            return [0,0]
-        hl, hr , ld, lr= 0,0,0,0
+            return [0,0,0]
+        hl, hr, ld, lr= 0,0,0,0 
         if root.left:
             lhl, lhr, ld = self.maxHeight(root.left)
             hl = max(lhl,lhr) + 1
@@ -19,6 +19,7 @@ class Solution:
         if root.right:
             rhl, rhr, lr = self.maxHeight(root.right)
             hr = max(rhl, rhr) + 1
-        res = [hl, hr, max(hl+hr, ld, lr)]
-        # print(f"root:{root.val} , return: {res}")
-        return res
+        
+        # third arg is the diameter
+        # it will be max of diameter of left, right subtree and subtree from current root
+        return [hl, hr, max(hl+hr, ld, lr)]
