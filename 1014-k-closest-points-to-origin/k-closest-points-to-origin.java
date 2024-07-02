e72b34af5b431aa915767e3387146da4adc84db2
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-07-01 22:21:39 -0400

    Time: 24 ms (74.84%) | Memory: 52.9 MB (48.75%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -2,19 +2,18 @@ class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int n = points.length;
         int[][] res = new int[k][2];
-        PriorityQueue<List<Integer>> q = new PriorityQueue<>((List<Integer> p1, List<Integer> p2)->Integer.compare(p1.get(0),p2.get(0)));
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>((int[] p1, int[] p2)->Integer.compare(p1[0],p2[0]));
         
         for(int i=0;i<n;i++){
             int x = points[i][0], y=points[i][1];
-            List<Integer> pair = new ArrayList<Integer>();
-            pair.add(x*x+y*y);
-            pair.add(i);
+            int[] pair = new int[]{x*x+y*y,i};
             q.add(pair);
         }
 
         for(int i=0;i<k;i++){
-            List<Integer> p = q.poll();
-            res[i] = points[p.get(1)];
+            int[] p = q.poll();
+            res[i] = points[p[1]];
         }
         return res;
     }
