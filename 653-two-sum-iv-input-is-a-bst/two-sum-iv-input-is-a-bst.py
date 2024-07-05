b948094722ef0f01fe8e4fcdf7ab3e6cb7a26f99
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-07-05 14:02:22 -0400

    Time: 61 ms (53.48%) | Memory: 18.4 MB (34.18%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -4,26 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class BSTIterator:
+    def __init__(self,root:Optional[TreeNode], reverse:bool=False):
+        self.reverse = reverse
+        self.stack = []
+        self.pushAll(root)
+    
+    def next(self):
+        if not self.hasNext(): return None
+        curr = self.stack.pop()
+        if self.reverse and curr.left:
+            self.pushAll(curr.left)
+        elif not self.reverse and curr.right:
+            self.pushAll(curr.right)
+        return curr.val
+    
+    def hasNext(self):
+        return len(self.stack)>0
+
+
+    def pushAll(self,root:Optional[TreeNode]):
+        temp = root
+        while temp:
+            self.stack.append(temp)
+            temp = temp.right if self.reverse else temp.left
+        
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        nums = []
-
-        def inorder(r):
-            if r:
-                if r.left:
-                    inorder(r.left)
-                nums.append(r.val)
-                if r.right:
-                    inorder(r.right)
-        
-        inorder(root)
-        l, r = 0, len(nums)-1
-        while l<r:
-            twoSum = nums[l]+nums[r]
+        l, r = BSTIterator(root), BSTIterator(root,True)
+        left, right = l.next(), r.next()
+        while left is not None and right is not None and left!=right:
+            # print(left,right)
+            twoSum = left + right
             if twoSum == k:
                 return True
             elif twoSum < k:
-                l+=1
+                left = l.next()
             else:
-                r-=1
+                right = r.next()
         return False
+        
