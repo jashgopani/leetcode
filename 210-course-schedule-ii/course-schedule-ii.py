6c617dc4a8e86ab95e84bd420557f2d278a23dda
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-07-17 22:13:04 -0400

    Time: 87 ms (75.84%) | Memory: 18.5 MB (18.22%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        visited = set()
+        stack = []
+        checked = [False]*numCourses
+        
+        adj = {i:[] for i in range(numCourses)}
+        for a,b in prerequisites:
+            adj[a].append(b)
+        
+        def dfs(a):
+            if a in visited: return False
+            stack.append(a)
+            if adj[a]==[]: return True
+
+            visited.add(a)
+            for b in adj[a]:
+                if not dfs(b): return False
+            visited.remove(a)
+
+            return True
+        res = []
+        for i in range(numCourses):
+            if not dfs(i): return []
+            adj[i]=[]
+            while stack:
+                course = stack.pop()
+                if not checked[course]:
+                    res.append(course)
+                    checked[course]=True
+        
+        return res
+        
