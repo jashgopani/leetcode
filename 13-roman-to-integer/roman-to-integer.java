495bfa6dfd14682f722215969f4a3bcf4dfe0fa2
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-08-14 18:45:10 -0400

    Time: 3 ms (80.87%) | Memory: 44.6 MB (49.94%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,4 +1,15 @@
 class Solution {
+    static Map<Character,Integer> mapping = new HashMap<>();
+    static {
+        mapping.put('I',1);
+        mapping.put('V',5);
+        mapping.put('X',10);
+        mapping.put('L',50);
+        mapping.put('C',100);
+        mapping.put('D',500);
+        mapping.put('M',1000);
+    }
+
     public int romanToInt(String s) {
         return tokenize(s);
     }
@@ -7,7 +18,7 @@ class Solution {
         int prev = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int current = tokens(s.charAt(i));
+            int current = mapping.get(s.charAt(i));
             
             if (prev < current) {
                 num += current - (2 * prev);
@@ -17,24 +28,5 @@ class Solution {
             prev = current;
         }
         return num;
-    }
-    public int tokens(char c) {
-        switch(c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-        }
-        return 0;
     }
 }
