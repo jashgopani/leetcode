df16b68a2b3e4808ec1e2d74bd41e1a8cc7b6adc
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2024-09-05 01:25:39 -0400

    Time: 11 ms (7.87%) | Memory: 60.1 MB (60.98%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int msum = 0;
+        for (int i:rolls){
+            msum+= i;
+        }
+
+        int targetSum = mean * (rolls.length+n);
+        int leftSum = targetSum - msum;
+
+        int[] res = new int[n];
+        for (int i=0;i<n;i++){
+            int q = leftSum / (n-i);
+            int r = leftSum % (n-i);
+            int v = Math.min(6, Math.max(1, q+r));
+            leftSum -= v;
+            res[i]=v;
+        }
+        if (leftSum != 0)
+            return new int[]{};
+        return res;
+    }
+}
