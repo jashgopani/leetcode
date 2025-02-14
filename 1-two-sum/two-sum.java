9f35b64f3247fe8e62371e7f879dd2a93571dc01
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-14 02:02:55 -0500

    Time: 4 ms (60.65%) | Memory: 44.7 MB (93.81%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,29 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return optimal(nums,target);
-    }
+        Map<Integer,Integer> pos = new HashMap<>();
 
-    public int[] optimal(int[] nums, int target){
-        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i< nums.length;i++){
+            pos.put(nums[i],i);
+        }
+
         for(int i=0;i<nums.length;i++){
-            if(!map.containsKey(target-nums[i])){
-                map.put(nums[i],i);
-            }else{
-                return new int[]{map.get(target-nums[i]),i};
+            int temp = target-nums[i];
+            if(pos.containsKey(temp) && pos.get(temp)!=i){
+                return new int[]{pos.get(temp),i};
             }
         }
-        return new int[]{0,1}; 
+
+        return new int[]{-1,-1};
     }
 
-    public int[] bruteForce(int[] nums, int target){
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int sum = nums[i]+nums[j];
-                if(sum==target){
-                    return new int[]{i,j};
-                }
-            }
-        }
-        return new int[]{0,1};
-    }
 }
