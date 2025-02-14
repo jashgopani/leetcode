0b6fd8d2bc42766adc5f193e14662ad6d2e0d4f9
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-14 17:54:55 -0500

    Time: 6 ms (97.96%) | Memory: 47.6 MB (94.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,87 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        return optimal(strs);
-    }
+        List<List<String>> ans = new ArrayList<>();
 
-    public List<List<String>> optimal(String[] strs){
-        
-        List<List<String>> groups = new ArrayList<>();
-        Map<String,List<String>> map = new HashMap<>();
+        Map<String,List<String>> freq = new HashMap<>();
 
-        for(String s:strs){
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String key = new String(charArray);
-            // String key = getCharCountString(s);
-            if(!map.containsKey(key)){
-                List<String> group = new ArrayList<>();
-                group.add(s);
-                map.put(key,group);
+        for(String s: strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String sorted = new String(c);
+            if(freq.containsKey(sorted)){
+                freq.get(sorted).add(s);
             }else{
-                map.get(key).add(s);
-            }
-        }
-        
-        for(List<String> l:map.values()){
-            groups.add(l);
-        }
-        return groups;
-    }
-
-    public String getCharCountString(String s){
-        int[] count = new int[26];
-        int a = (int)'a';
-        for(char c:s.toCharArray()){
-            count[((int)c)-a]+=1;
-        }
-        return Arrays.toString(count);
-    }
-
-    public List<List<String>> bruteForce(String[] strs){
-        //O(N^3)
-        Set<Integer> grouped = new HashSet<>();
-        List<List<String>> groups = new ArrayList<>();
-        for(int i=0;i<strs.length;i++){
-            if(!grouped.contains(i)){
-                List<String> group = new ArrayList<>();
-                group.add(strs[i]);
-                for(int j=i+1;j<strs.length;j++){
-                    if(!grouped.contains(j) && isAnagram(strs[i],strs[j])){
-                        group.add(strs[j]);
-                        grouped.add(j);
-                    }
-                }
-                groups.add(group);
-            }
-        }
-        return groups;    
-    }
-
-    public boolean isAnagram(String s, String t) {
-        // O(N)
-        if(s.length()!=t.length())
-            return false;
-        
-        Map<Character,Integer> charCount = new HashMap<>();
-        for(char c:s.toCharArray()){
-            if(charCount.containsKey(c)){
-                charCount.put(c,charCount.get(c)+1);
-            }else{
-                charCount.put(c,1);
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                freq.put(sorted,list);
             }
         }
 
-        for(char c:t.toCharArray()){
-            if(charCount.containsKey(c)){
-                charCount.put(c,charCount.get(c)-1);
-                if(charCount.get(c)==0){
-                    charCount.remove(c);
-                }
-            }else{
-                return false;
-            }
+        for(Map.Entry<String, List<String>> e: freq.entrySet()){
+            String key = e.getKey();
+            List<String> value = e.getValue();
+
+            ans.add(value);
         }
 
-        return charCount.isEmpty();
+        return ans;
+
+
     }
 }
