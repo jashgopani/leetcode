2f53ccd247f4257022e0a4b4720d2c6444438881
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-15 15:56:46 -0500

    Time: 2 ms (88.04%) | Memory: 55.7 MB (42.46%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,28 +1,38 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int product = 1;
-        int zeroIndex = -1;
-        boolean allZeros = false;
+        return optimal(nums);
+    }
+
+    public int[] optimal(int[] nums){
+        int[] ans = new int[nums.length];
+
+        for(int i=0, product=1;i<ans.length;i++){
+            if(i>0) product *= nums[i-1];
+            ans[i] = product;
+        }
+
+        
+        for(int i=ans.length-1, product=1;i>=0;i--){
+            if(i < ans.length-1) product*= nums[i+1];
+            ans[i] *= product;
+        }   
+
+        return ans;
+    }
+
+
+    public int[] bruteForce(int[] nums) {
+        int[] ans = new int[nums.length];
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0 && zeroIndex==-1){
-                zeroIndex=i;
-            }else if(nums[i]==0 && zeroIndex!=-1){
-                allZeros = true;        
-            }else{
-                product*=nums[i];
+            ans[i] = 1;
+            for(int j=0;j<nums.length;j++){
+                if(i!=j){
+                    ans[i] *= nums[j];
+                }
             }
         }
 
-        int[] res = new int[nums.length];
-        if(!allZeros && zeroIndex==-1){
-            for(int i=0;i<res.length;i++){
-                res[i]=(product/nums[i]);
-            }
-        }else if(!allZeros && zeroIndex!=-1){
-            res[zeroIndex] = product;
-        }
-        
-        return res;
+        return ans;
     }
 }
