0cb1dc21f9364961c9bf7e0e8a7b59d6614959e1
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-15 17:16:35 -0500

    Time: 4 ms (37.89%) | Memory: 45.4 MB (6.88%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,84 +1,40 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return optimized(board);
+        int n = board.length;
+        int gridSize = 3;
+
+        Map<Integer,Set<Character>> gridMap = new HashMap<>();
+        Map<Integer,Set<Character>> rowMap = new HashMap<>();
+        Map<Integer, Set<Character>> colMap = new HashMap<>();
+
+        //traverse entire board to fill in the maps
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                char x = board[i][j];
+                
+                if (x=='.') continue;
+
+                int gridRow = i/gridSize;
+                int gridCol = j/gridSize;
+                int gridNo = gridSize * gridRow + gridCol;
+
+                gridMap.putIfAbsent(gridNo, new HashSet<>());
+                if(gridMap.get(gridNo).contains(x)) return false;
+                gridMap.get(gridNo).add(x);
+
+                rowMap.putIfAbsent(i,new HashSet<>());
+                if(rowMap.get(i).contains(x)) return false;
+                rowMap.get(i).add(x);
+
+                colMap.putIfAbsent(j,new HashSet<>());
+                if(colMap.get(j).contains(x)) return false;
+                colMap.get(j).add(x);
+            }
+        }
+
+        return true;
     }
     
-    public boolean bruteForce(char[][] board){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                if(board[i][j]!='.'){
-                    System.out.printf("Checking: (%d,%d)\n",i,j);
-                    
-                    //check in same row
-                    for(int y=0;y<board[i].length;y++){
-                        if(j!=y && board[i][j]==board[i][y]){
-                            System.out.printf("\tSame row entry @ (%d,%d)\n",i,y);
-                            return false;
-                        }
-                    }
-                    
-                    //check in same col
-                    for(int x=0;x<board.length;x++){
-                        if(i!=x && board[i][j]==board[x][j]){
-                            System.out.printf("\tSame col entry @ (%d,%d)\n",x,j);
-                            return false;
-                        }
-                    }
-
-                    //check subgrid
-                    int si = (i/3)*3;
-                    int sj = (j/3)*3;
-                    System.out.printf("\t Subgrid :: (%d,%d)\n",si,sj);
-                    for(int p=si;p<(si+3);p++){
-                        for(int q=sj;q<(sj+3);q++){
-                            if(p!=i && q!=j && board[p][q]==board[i][j]){
-                                System.out.printf("\t\tSame subgrid entry @ (%d,%d)\n",p,q);
-                                return false;
-                            }
-                        }
-                    }
-                }
-
-            }
-        }
-
-        return true;
-    }
-
-    public boolean optimized(char[][] board){
-        HashMap<Integer,Set<Character>> rowMap = new HashMap<>();
-        HashMap<Integer,Set<Character>> colMap = new HashMap<>();
-        HashMap<String,Set<Character>> sgMap = new HashMap<>();
-        
-        for(int i=0;i<board.length;i++){
-            rowMap.putIfAbsent(i,new HashSet<>());
-            for(int j=0;j<board[i].length;j++){
-                if(board[i][j]!='.'){
-                    int sgTop = (i/3);
-                    int sgLeft = (j/3);
-                    String sgKey = String.format("%d%d",sgTop,sgLeft);
-
-                    colMap.putIfAbsent(j,new HashSet<>());
-                    sgMap.putIfAbsent(sgKey,new HashSet<>());
-
-                    char currentValue = board[i][j];
-
-                    if(rowMap.get(i).contains(currentValue)
-                        || colMap.get(j).contains(currentValue)
-                        || sgMap.get(sgKey).contains(currentValue)){
-                        return false;
-                    }
-                    
-                    rowMap.get(i).add(currentValue);
-                    colMap.get(j).add(currentValue);
-                    sgMap.get(sgKey).add(currentValue);
-                    
-                    
-                }
-            }
-        }
-
-        return true;
-    }
+   
 
 }
