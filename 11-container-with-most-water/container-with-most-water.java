2ecd95aca35c5817c4aca7194ebccd3ef87d2b3a
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-16 10:20:18 -0500

    Time: 5 ms (74.41%) | Memory: 56.8 MB (98.51%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,31 +1,18 @@
 class Solution {
     public int maxArea(int[] height) {
-        return optimal(height);
-    }
+        int l = 0, r = height.length-1, maxArea = 0;
+        while(l<r){
+            int h = Math.min(height[l],height[r]);
+            int w = r-l;
+            int area = w*h;
+            maxArea = Math.max(area,maxArea);
 
-    public int bruteForce(int[] height){
-        int area = 0;
-        for(int i=0;i<height.length;i++){
-            for(int j=i+1;j<height.length;j++){
-                int y = Math.min(height[i],height[j]);
-                int x = Math.abs(i-j);
-                area = Math.max(area,x*y);
-            }
-        }
-        return area;
-    }
-
-    public int optimal(int[] height){
-        int max = 0;
-        for(int l=0,r=height.length-1;l<r;){
-            int area = (r-l)*Math.min(height[l],height[r]);
-            max = Math.max(area,max);
-            if(height[l]<height[r]){
-                l++;
-            }else{
+            if(height[l] >= height[r]){
                 r--;
+            }else{
+                l++;
             }
         }
-        return max;
+        return maxArea;
     }
 }
