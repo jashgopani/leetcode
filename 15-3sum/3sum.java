8ebe41e295252e27198e310188ac074d49c1d566
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-16 10:00:20 -0500

    Time: 46 ms (27.97%) | Memory: 52.7 MB (19.27%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,44 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int arr[] = nums.clone();
         List<List<Integer>> res = new ArrayList<>();
 
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            int a = nums[i];
-            if (i > 0 && a == nums[i - 1])
-                continue;
-            else {
-                for (int l = i+1, r = nums.length - 1; l < r;) {
-                    int b = nums[l];
-                    int c = nums[r];
+        Arrays.sort(arr);
 
-                    int threeSum = a + b + c;
-                    
-                    if (threeSum == 0) {
-                        List<Integer> tuple = list(a, b, c);
-                        res.add(tuple);
-                        // for finding other two sum pairs
-                        l++;
-                        while (l < r && nums[l] == b)
-                            l++;
-                    } else if (threeSum < 0) {
-                        l++;
-                    } else {
-                        r--;
-                    }
+        for(int i=0;i<arr.length;i++){
+            int a = arr[i];
+
+            if(i>0 && a==arr[i-1])continue;
+
+            for(int l=i+1, r=arr.length-1;l<r;){
+                int b = arr[l];
+                int c = arr[r];
+
+                int threeSum = a+b+c;
+
+                if(threeSum==0){
+                    List<Integer> tuple = new ArrayList<>();
+                    tuple.add(a);
+                    tuple.add(b);
+                    tuple.add(c);
+                    res.add(tuple);
+                    while(l<r && arr[l]==b) l++;
+                }else if(threeSum < 0){
+                    while(l<r && arr[l]==b) l++;
+                }else{
+                    r--;
+                    while(l<r && arr[r]==c) r--;
                 }
             }
         }
 
         return res;
-    }
-
-    public List<Integer> list(int... nums) {
-        List<Integer> l = new ArrayList<>();
-        for (int i : nums) {
-            l.add(i);
-        }
-
-        return l;
     }
 }
