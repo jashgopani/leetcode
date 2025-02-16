a347e9b643c9d263b004c55fd0de0c32ac915890
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-16 12:13:51 -0500

    Time: 16 ms (45.41%) | Memory: 44.7 MB (82.66%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -7,12 +7,22 @@ class Solution {
         int l = 0;
         for(int r=0; r < s.length();r++){
             freq[s.charAt(r)-'A']++;
-            if((r-l+1)-Arrays.stream(freq).max().orElseThrow() > k){
+            if((r-l+1)-max(freq) > k){
                 freq[s.charAt(l)-'A']--;
                 l++;
             }
             ans = Math.max(ans, r-l+1);
         }
         return ans;
+    }
+
+    public int max(int []arr){
+        int ans = Integer.MIN_VALUE;
+        for(int i: arr){
+            ans = i > ans? i: ans;
+        }
+
+        return ans;
+
     }
 }
