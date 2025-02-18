580850bccd02f5abee7c7b5cfa2e36f1bba9e569
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-18 18:41:45 -0500

    Time: 56 ms (21.88%) | Memory: 45.2 MB (33.25%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -3,27 +3,22 @@ class Solution {
             int n=s.length();
             if(n==1) return 1;
 
-            List<String> set = new ArrayList<>();
             int count = 0;
             
             for(int i=0;i<n;i++){
                 //odd palindromes
-                count += addPalindromes(i,i,s,set);
+                count += addPalindromes(i,i,s);
                 //even palindromes
-                count += addPalindromes(i,i+1,s,set);
+                count += addPalindromes(i,i+1,s);
             }
-        // return set.size();
         return count;
     }
     
-    public int addPalindromes(int l, int r, String s, List<String> set){
+    public int addPalindromes(int l, int r, String s){
         int count = 0;
         while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
             String substr = s.substring(l,r+1);
-            // if(!set.contains(substr)){
-                // set.add(substr);
-                count++;
-            // }
+            count++;
             l--;
             r++;
         }
