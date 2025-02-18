ba31c40984dc062b90bbfb18b56da20aebe08f62
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2025-02-18 16:37:23 -0500

    Time: 0 ms (100.00%) | Memory: 40.9 MB (79.05%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -6,14 +6,16 @@ class Solution {
     }
 
     public int robHouses(int[] nums, int start, int end){
-        int prev = 0, curr = 0;
+        int rob1 = 0, rob2 = 0;
+        // rob1: The maximum money that could have been robbed up to the previous house (i.e., before considering the current house).
+        // rob2: The maximum money that could have been robbed up to the current house (i.e., including the current house).
 
         for(int i=start; i<end; i++){
-            int maxRob = Math.max(prev+nums[i], curr);
-            prev = curr;
-            curr = maxRob;
+            int maxRob = Math.max(rob1+nums[i], rob2);
+            rob1 = rob2;
+            rob2 = maxRob;
         }
 
-        return curr;
+        return rob2;
     }
 }
